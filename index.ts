cc70057ebbc02bac34ce3d2a1c3b89a22f91cbe9
nitish singh <nitishsingh925@gmail.com>
2024-04-15 21:17:18 +0530

    add route

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
+// const server = Bun.serve({
+//   port: 3000,
+//   fetch(request) {
+//     return new Response("Welcome to Bun!");
+//   },
+// });
+
+const port = process.env.PORT || 5000;
+
 const server = Bun.serve({
-  port: 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
+  port: port || 5000,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/blog") return new Response("Blog!");
+    return new Response("404!");
   },
 });
-
-console.log(process.env.FOO);
+console.log(port);
 
 console.log(`Listening on localhost:${server.port}`);
